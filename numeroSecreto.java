699db18d21e4932f530d7ef6528971e50b391e1e
lara-gs <lara_989@hotmail.com>
2024-09-26 11:41:22 +0200

    Juego Número Secreto

---
+++ numeroSecreto.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class numeroSecreto {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numSecreto;
+        int intentos = 0;
+        boolean haAcertado = false;
+
+        numSecreto = (int) (Math.random() * 100);
+        
+        //Aqui le pedimos al usuario que meta el numero
+        System.out.println("Intenta adivinar el numero secreto! (pista: esta entre el 0 y el 100)");
+
+
+        while (!haAcertado) { //usamos while para que siga pidiendo el numero hasta que acierte 
+            try {
+                System.out.println("Introduce tu numero o pulsa 's' para reiniciar la partida ");
+                String valorEntrada = input.next();
+
+                if (valorEntrada.equals("s")) { //Para reiniciar 
+                    
+                    numSecreto = (int) (Math.random() * 100); //Con esto sacamos un nuevo numero aleatorio al reiniciarlo
+                    intentos = 0;
+                    System.out.println("Empiezas de nuevo (RECUERDA: esta entre el 0 y el 100)");
+                } else {
+
+                    int numElegido = Integer.parseInt(valorEntrada);
+                    if (numElegido >= 0 && numElegido <= 100) { //Para que solo puedan meter numeros entre 0 y 100
+                        intentos = intentos + 1; //Con esto conseguimos que le vaya sumando el numero de intentos
+
+                        if (numElegido < numSecreto) { //Comprueba si el numero que nos dan es menor al numero secreto
+                            System.out.println("El numero secreto es mayor");
+                            System.out.println("Llevas " + intentos + " intento/s");
+                        } else if (numElegido > numSecreto) { //Comprueba si el numero que nos dan es mayor al numero secreto
+                            System.out.println("El numero secreto es menor");
+                            System.out.println("Llevas " + intentos + " intento/s");
+                        } else {
+                            System.out.println("¡¡Has acertado!! El numero secreto era " + numSecreto);
+                            System.out.println("Lo has conseguido en " + intentos + " intento/s");
+
+                            haAcertado = true; // cuando lo acierte para parar el bucle
+                        }
+                    }
+                }
+                //Para que le salga este mensaje de error si no introduce un numero valido
+            } catch (Exception e) { 
+                System.out.println("El valor introducido no es valido");
+                
+            }
+        }
+        input.close();
+    }
+
+}
